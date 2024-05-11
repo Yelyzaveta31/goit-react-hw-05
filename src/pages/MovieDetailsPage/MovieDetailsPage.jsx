5df8d0c4263aeffc068e161cd642c8974f4cd248
Yelyzaveta31 <Didenkoelizabeth@gmail.com>
2024-05-11 19:14:29 +0300

    update

--- src/pages/MovieDetailsPage/MovieDetailsPage.jsx
+++ src/pages/MovieDetailsPage/MovieDetailsPage.jsx
@@ -1,9 +1,75 @@
-import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { Link, NavLink, Outlet, useParams } from "react-router-dom";
+import { fetchMovieById } from "../../service/Api";
+import s from "./MovieDetailsPage.module.css";
 
 const MovieDetailsPage = () => {
   const { movieId } = useParams();
+  const [movie, setMovie] = useState("");
+  useEffect(() => {
+    try {
+      const getMovie = async () => {
+        const results = await fetchMovieById(movieId);
+        setMovie(results);
+      };
+      getMovie();
+    } catch (error) {
+      console.log(error);
+    }
+  }, [movieId]);
 
-  return <div>{movieId}</div>;
+  if (!movie) return <h3>Loading...</h3>;
+  const year = movie.release_date.split("-")[0];
+
+  return (
+    <div className={s.container}>
+      <Link className={s.goBack} to="/">
+        {" "}
+        Go Back{" "}
+      </Link>
+      <div className={s.wrapper}>
+        <img
+          className={s.image}
+          src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+          alt={movie.title}
+        />
+        <div className={s.content}>
+          <div>
+            <h1 className={s.title}>
+              {movie.title} ({year})
+            </h1>
+            <p className={s.text}>User Score: {movie.vote_average}</p>
+          </div>
+          <div>
+            <h2 className={s.titleOverview}>Overview</h2>
+            <p className={s.text}>{movie.overview}</p>
+          </div>
+          <div>
+            <h3 className={s.titleGenre}>Genres</h3>
+            <ul className={s.list}>
+              {movie.genres.map((genre) => {
+                return (
+                  <li className={s.item} key={genre.id}>
+                    {genre.name}
+                  </li>
+                );
+              })}
+            </ul>
+          </div>
+        </div>
+      </div>
+      <nav className={s.navigation}>
+        <h3 className={s.titleGenre}>Additional Information</h3>
+        <NavLink className={s.link} to="cast">
+          Cast
+        </NavLink>
+        <NavLink className={s.link} to="reviews">
+          Reviews
+        </NavLink>
+      </nav>
+      <Outlet />
+    </div>
+  );
 };
 
 export default MovieDetailsPage;
