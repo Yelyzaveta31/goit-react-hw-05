969918feb7161c102dd98c2bd54553fef6354ac8
Yelyzaveta31 <Didenkoelizabeth@gmail.com>
2024-05-12 21:41:32 +0300

    update

--- src/service/Api.js
+++ src/service/Api.js
@@ -15,7 +15,6 @@ export const fetchMovies = async () => {
 export const fetchMovieById = async (id) => {
   const movieUrl = `${baseUrl}/movie/${id}?api_key=${API_KEY}`;
   const { data } = await axios.get(movieUrl);
-  console.log(data);
   return data;
 };
 export const fetchCast = async (id) => {
